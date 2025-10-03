84e03fb2418f74e2248efc9abfefe1d4304299f3
supichaprom-cell <supicha.prom@bumail.net>
2025-10-03 19:16:41 +0700

    homwork1

--- javascripts/testjs.js
+++ javascripts/testjs.js
@@ -1,1 +1,19 @@
-alert('in js floder');
+let userInput = prompt("Input integer number");
+ 
+ 
+let number = parseInt(userInput);
+ 
+ 
+if (isNaN(number)) {
+    alert("Please input a valid integer number");
+} else {
+    if (number === 0) {
+        alert("zero integer number");
+    } 
+    else if (number % 2 === 0) {
+        alert("even integer number");
+    } 
+    else {
+        alert("odd integer number");
+    }
+}
